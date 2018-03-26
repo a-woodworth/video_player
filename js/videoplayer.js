5d3aeed262b551af91db3407ac2c88feb1f6a30f
Amy Woodworth <woodworthcommunications@gmail.com>
2018-03-26 15:37:38 -0500

    Break up features section in mediaplayer to multiple lines.

--- js/videoplayer.js
+++ js/videoplayer.js
@@ -2,7 +2,13 @@
 $('video').mediaelementplayer({
   pluginPath: 'https://cdnjs.com/libraries/mediaelement/',
   shimScriptAccess: 'always',
-  features: ['playpause', 'current', 'progress', 'duration', 'volume', 'fullscreen'],
+  features: [
+    'playpause',
+    'current',
+    'progress',
+    'duration',
+    'volume',
+    'fullscreen'],
   videoWidth: '100%',
   videoHeight: '100%',
   enableAutosize: true,
